4cac48b7d1d45f24bea4e72f355ed33df257d67f
Eamonn de Leastar <edeleastar@wit.ie>
2017-03-23 18:07:55 +0000

    signup route + action in the accounts controller

--- app/controllers/Accounts.java
+++ app/controllers/Accounts.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import models.Member;
 import play.Logger;
 import play.mvc.Controller;
 
@@ -13,5 +14,13 @@ public class Accounts extends Controller
   public static void login()
   {
     render("login.html");
+  }
+
+  public static void register(String firstname, String lastname, String email, String password)
+  {
+    Logger.info("Registering new user " + email);
+    Member member = new Member(firstname, lastname, email, password);
+    member.save();
+    redirect("/");
   }
 }
